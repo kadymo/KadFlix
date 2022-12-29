c6ffc728a3d1eabf4c910e9ff361b9a828ea24b1
Kádymo Santana <santanakadymo@gmail.com>
2022-12-29 21:53:31 +0000

    refactor: Improving Property Naming

--- kadflix/src/store/index.js
+++ kadflix/src/store/index.js
@@ -6,15 +6,15 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     movies: "",
-    movies_search: "",
-    movie_search: "",
+    searched_movies: "",
+    searched_movie: "",
     api_images: "https://image.tmdb.org/t/p/",
-    favorite_movies: []
+    favorite_movies: [],
   },
 
   mutations: {
-    CHANGE_MOVIES_SEARCH(state, payload) {
-      state.movies_search = payload;
+    GET_SEARCHED_MOVIES(state, payload) {
+      state.searched_movies = payload;
     },
 
     FAVORITE_MOVIE(state, payload) {
@@ -24,7 +24,7 @@ export default new Vuex.Store({
     DISFAVOR_MOVIE(state, payload) {
       const index = state.favorite_movies.indexOf(payload);
       const movieObject = {
-        index: index
+        index: index,
       };
       movieObject.index === -1 ? (movieObject.index = 0) : null;
       state.favorite_movies.splice(movieObject.index, 1);
@@ -32,18 +32,18 @@ export default new Vuex.Store({
 
     UPDATE_LS(state, payload) {
       state.favorite_movies = payload;
-    }
+    },
   },
 
   actions: {
-    fetchSearchMovies(context, payload) {
+    fetchSearchedMovies(context, payload) {
       fetch(
         `https://api.themoviedb.org/3/search/movie?api_key=37823c25fd81a1efa9124efeb53be3a8&language=pt-BR&query=${payload}`
       )
         .then((r) => r.json())
         .then((r) => {
-          context.commit("CHANGE_MOVIES_SEARCH", r);
+          context.commit("GET_SEARCHED_MOVIES", r);
         });
-    }
-  }
+    },
+  },
 });
