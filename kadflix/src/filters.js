2b7c45e5405c1346a8793311cfd073f24aa972c3
Kádymo Santana <santanakadymo@gmail.com>
2022-12-29 21:50:41 +0000

    refactor: Improving the Date Filter

--- kadflix/src/filters.js
+++ kadflix/src/filters.js
@@ -1,5 +1,17 @@
-export function dateFilter(value) {
-  return value.split("-").reverse().join("/");
+export function dateFilter(value, type) {
+  if (value) {
+    const date = new Date(value);
+
+    if (type === "long") {
+      return date.toLocaleDateString("pt-BR", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    } else if (type === "short") {
+      return date.toLocaleDateString("pt-BR");
+    }
+  }
 }
 
 export function genreFilter(value) {
